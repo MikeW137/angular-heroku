e346e3b461f6dbce81e52c474e747d60d5e681c3
Michael Wydra <wydrama@gmail.com>
2021-05-10 16:59:14 -0500

    added category service

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -30,6 +30,9 @@ export class UserService {
         localStorage.setItem('currentUser', `${user.email}`);
         localStorage.setItem('token', `${token}`);
         console.log(response, token);
+        this.currentUser = user.email;
+        this.searchSubject.next(this.currentUser);
+        this.router.navigate(['/categories']);
       }, err => console.log(err));
   }
 
